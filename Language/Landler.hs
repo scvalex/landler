f5fc809d05dd60ee2ac18633a171c49f6a481cc5
Alexandru Scvortov <scvalex@gmail.com>
2011-11-11 18:02:47 +0000

    simple lambda-calculus evaluator

--- Language/Landler.hs
+++ Language/Landler.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables #-}
+
 module Language.Landler (
-        foo
+        Term(..), Var,
+        step, dance
     ) where
 
-foo :: ()
-foo = ()
+import Data.Typeable ( Typeable )
+import Text.Interpol ( (^-^) )
+
+type Var = String
+
+data Term = Var Var | Ab Var Term | App Term Term
+          deriving ( Eq, Typeable )
+
+instance Show Term where
+    show (Var v)   = v
+    show (Ab v t)  = "(\\" ^-^ v ^-^ ". " ^-^ t ^-^ ")"
+    show (App t p) = "" ^-^ t ^-^ " " ^-^ p
+
+step :: Term -> Maybe Term
+step (App (Ab vx vm) vn) = Just $ repl vm vx vn
+    where
+      repl :: Term -> Var -> Term -> Term
+      repl t@(Ab y m) x n
+          | x == y    = t
+          | otherwise = Ab y (repl m x n)
+      repl (App t p) x n = App (repl t x n) (repl p x n)
+      repl t@(Var x) y n
+          | x == y    = n
+          | otherwise = t
+step _ = Nothing
+
+dance :: Term -> [Term]
+dance t = case step t of
+            Nothing -> [t]
+            Just t' -> t : dance t'
