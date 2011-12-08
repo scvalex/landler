ffed0b0c1b316d25c981e8280ef71645204b601d
Alexandru Scvortov <scvalex@gmail.com>
2011-12-08 15:44:39 +0000

    flesh-out REPL

--- Main.hs
+++ Main.hs
@@ -4,9 +4,13 @@ import Control.Monad.IO.Class ( liftIO )
 import System.Console.Haskeline ( InputT, runInputT
                                 , Settings(..), defaultSettings
                                 , getInputLine )
-import Language.Landler ( Step, run )
+import Language.Landler ( Term, Var
+                        , Step, run, breakDance
+                        , Statement(..), parseStatement )
 import System.Environment ( getArgs )
 import Text.Interpol ( (^-^) )
+
+type Environment = [(Var, Term)]
 
 main :: IO ()
 main = do
@@ -18,10 +22,10 @@ main = do
          (_, stepss) <- run fn
          putStrLn $ concatMap prettyPrint stepss
     _ ->
-        error "Well, fsck.  I didn't think this could happen."
+        error "FIXME Use cmdargs or something."
 
 runInterpreter :: IO ()
-runInterpreter = printBanner >> runInputT settings loop
+runInterpreter = printBanner >> runInputT settings (loop [])
     where
       printBanner = putStrLn "landler, version <VERSION>: \
                              \https://github.com/scvalex/landler  \
@@ -29,15 +33,23 @@ runInterpreter = printBanner >> runInputT settings loop
 
       settings = defaultSettings { historyFile = Just "~/.landler_history" }
 
-      loop :: InputT IO ()
-      loop = do
-        minput <- getInputLine "∅ > "
+      loop :: Environment -> InputT IO ()
+      loop env = do
+        let envKeys = case unwords $ map fst env of
+                        "" -> "∅"
+                        ek -> ek
+        minput <- getInputLine (envKeys ^-^ " > ")
         case minput of
           Nothing    -> return ()
-          Just input -> handleStatement input >> loop
+          Just input -> handleStatement env input >>= loop
 
-      handleStatement :: String -> InputT IO ()
-      handleStatement = liftIO . putStrLn
+      handleStatement :: Environment -> String -> InputT IO Environment
+      handleStatement env str = do
+        let stmt = parseStatement str
+        case stmt of
+          Let v t -> return $ (v, t) : env
+          Call t -> liftIO (putStrLn . prettyPrint $ breakDance env t) >>
+                    return env
 
 prettyPrint :: [Step] -> String
 prettyPrint = unlines . go
