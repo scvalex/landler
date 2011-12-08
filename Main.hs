8e32901d0f0fbfc96f996e587838910f4b84d25a
Alexandru Scvortov <scvalex@gmail.com>
2011-12-08 01:53:20 +0000

    add stubs for a REPL

--- Main.hs
+++ Main.hs
@@ -1,14 +1,43 @@
 module Main where
 
+import Control.Monad.IO.Class ( liftIO )
+import System.Console.Haskeline ( InputT, runInputT
+                                , Settings(..), defaultSettings
+                                , getInputLine )
 import Language.Landler ( Step, run )
 import System.Environment ( getArgs )
 import Text.Interpol ( (^-^) )
 
 main :: IO ()
 main = do
-  [fn] <- getArgs
-  (_, stepss) <- run fn
-  putStrLn $ concatMap prettyPrint stepss
+  args <- getArgs
+  case args of
+    [] ->
+        runInterpreter
+    [fn] -> do
+         (_, stepss) <- run fn
+         putStrLn $ concatMap prettyPrint stepss
+    _ ->
+        error "Well, fsck.  I didn't think this could happen."
+
+runInterpreter :: IO ()
+runInterpreter = printBanner >> runInputT settings loop
+    where
+      printBanner = putStrLn "landler, version <VERSION>: \
+                             \https://github.com/scvalex/landler  \
+                             \:? for help"
+
+      settings = defaultSettings { historyFile = Just "~/.landler_history" }
+
+      loop :: InputT IO ()
+      loop = do
+        minput <- getInputLine "∅ > "
+        case minput of
+          Nothing    -> return ()
+          Just input -> handleStatement input >> loop
+
+      handleStatement :: String -> InputT IO ()
+      handleStatement = liftIO . putStrLn
 
 prettyPrint :: [Step] -> String
 prettyPrint = unlines . go
